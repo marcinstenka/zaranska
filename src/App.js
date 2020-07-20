2975feb5c5ce1aef09ecd18a467ce7ba2a4575bb
Marcin Stenka <mastenka@wp.pl>
2020-07-20 12:25:57 +0200

    Initial commit from laptop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1 className='extender'>Natalia Zarańska</h1>;
+  return <h1 className='extender'>Natalia Zarańska 1.1</h1>;
 }
 
 export default App;
