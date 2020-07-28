03f4606d5c9ef57bbb76071abe12b089eba09753
marcinstenka <mastenka@wp.pl>
2020-07-28 10:58:10 +0200

    Updates

---
+++ precache-manifest.1166488f9be0a9af84be984965517547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640bc82c071b8ad7878f9ae3bf872d02",
+    "url": "/zaranska/index.html"
+  },
+  {
+    "revision": "d7efe384e452ceb61ddb",
+    "url": "/zaranska/static/css/main.b4e444f4.chunk.css"
+  },
+  {
+    "revision": "002c9d62a4f5e2606330",
+    "url": "/zaranska/static/js/2.99b0ee5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zaranska/static/js/2.99b0ee5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7efe384e452ceb61ddb",
+    "url": "/zaranska/static/js/main.4fdc20a4.chunk.js"
+  },
+  {
+    "revision": "7d6252aaf7481747b232",
+    "url": "/zaranska/static/js/runtime-main.95117ec8.js"
+  },
+  {
+    "revision": "456605e7442b625b91c5e171d3cc2920",
+    "url": "/zaranska/static/media/shape-right.456605e7.png"
+  }
+]);
