c2ab766d70e923f2ce0dceaf9f8dc92ba8d070c0
marcinstenka <mastenka@wp.pl>
2020-07-29 09:06:43 +0200

    hero and contact 1.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <ul className='top-bar__desktop-nav'>
-            <li className='desktop-nav__item'><Link to='/'>NATALIA</Link></li>
+            <li className='desktop-nav__item'><Link to='/'>STRONA GŁÓWNA</Link></li>
             <li className='desktop-nav__item'><Link to='/'>O MNIE</Link></li>
             <li className='desktop-nav__item'><Link to='/'>OFERTA</Link></li>
             <li className='desktop-nav__item'><Link to='/'>BLOG</Link></li>
