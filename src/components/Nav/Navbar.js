cf4a0f3f41bd2b9bef8bd6e5708fd9ed457b74d1
marcinstenka <mastenka@wp.pl>
2020-08-20 21:33:36 +0200

    mobile first refacturing 1.1

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <ul className='top-bar__desktop-nav'>
-            <li className='desktop-nav__item'><Link to='/'>STRONA GŁÓWNA</Link></li>
-            <li className='desktop-nav__item'><Link to='/'>O MNIE</Link></li>
-            <li className='desktop-nav__item'><Link to='/'>OFERTA</Link></li>
-            <li className='desktop-nav__item'><Link to='/'>BLOG</Link></li>
-            <li className='desktop-nav__item'><Link to='/kontakt'>KONTAKT</Link></li>
-        </ul>
+        <div className='top-bar__desktop-nav'>
+            <div className='desktop-nav__item'><Link to='/'>STRONA GŁÓWNA</Link></div>
+            <div className='desktop-nav__item'><Link to='/'>O MNIE</Link></div>
+            <div className='desktop-nav__item'><Link to='/'>OFERTA</Link></div>
+            <div className='desktop-nav__item'><Link to='/'>BLOG</Link></div>
+            <div className='desktop-nav__item'><Link to='/kontakt'>KONTAKT</Link></div>
+        </div>
     )
 }
 
