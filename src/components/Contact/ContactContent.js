eb3c17b2096028493ee2ffd784df93cfdc6ac960
Marcin Stenka <mastenka@wp.pl>
2020-09-29 11:36:51 +0200

    Deploy on hosting 1.0

--- src/components/Contact/ContactContent.js
+++ src/components/Contact/ContactContent.js
@@ -9,7 +9,7 @@ function ContactContent() {
             <img className="contact-shape" src={ContactShape} alt='Dekoracja na stronie'/>
             <img className="mobile-contact-shape" src={MobileContactShape} alt='Dekoracja na stronie'/>
             <div className="contact" data-aos="zoom-in">
-                <div className="contact__map"><iframe title='mapa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2314.411412502009!2d17.748591415883947!3d54.54387268024744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fddd53e65ca03b%3A0x94ccc8576b453e8f!2sPlac%20Pokoju%209%2C%2084-351%20L%C4%99bork!5e0!3m2!1spl!2spl!4v1595932339897!5m2!1spl!2spl" frameBorder={'0'}
+                <div className="contact__map"><iframe title='mapa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2314.0103004250072!2d17.791033215884198!3d54.550943880249065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fdc2bf1b2b5b27%3A0x2f90741ed2c06d06!2sD%C5%82uga%2024%2C%2084-353!5e0!3m2!1spl!2spl!4v1601372090479!5m2!1spl!2spl" frameBorder={'0'}
         width={'100%'}
         height={'100%'} ></iframe></div>
                 <div data-aos="fade-up" className="contact__info">
