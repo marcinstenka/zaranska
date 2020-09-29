4261f35661ab9d79a8655aeb23cf416e4ec88b82
Marcin Stenka <mastenka@wp.pl>
2020-09-29 21:50:56 +0200

    blank

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Fonts
+import './fonts/AnticDidone-Regular.otf'
+import './fonts/Intervogue Soft Medium.otf'
+import './fonts/Intervogue Soft Regular.otf'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
